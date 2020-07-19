35244362f6ecfad6d0bf0f3b83738012320c0b96
Karl Lemfon <karllemfon@gmail.com>
2020-07-19 11:32:30 +0000

    Initial Settings, setting up the various pages and services. A service has been created to manage saving the checklists and retrieving them from storage.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,14 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'intro',
+    loadChildren: () => import('./intro/intro.module').then( m => m.IntroPageModule)
+  },
+  {
+    path: 'checklist',
+    loadChildren: () => import('./checklist/checklist.module').then( m => m.ChecklistPageModule)
+  },
 ];
 
 @NgModule({
